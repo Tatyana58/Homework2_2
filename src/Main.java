7beb0ae8aa5840a8745a10966c3dd90693d6cfca
Tatyana58 <tatbir@rambler.ru>
2022-11-12 17:46:29 +0300

    types of variables Task 2 Task 3 Task 4 Task 5

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,23 @@ public class Main {
         int billion = 1000000000;
         byte hundred = 100;
         short thousand = 30000;
-        long trillion = 400L;
+        long trillion = 4000000000L;
         float smallDecimal =-0.1f;
         double bigDecimal = 0.6666666;
+        System.out.println("Задание 1 ");
         System.out.println("Значение переменной billion с типом int равно " + billion);
         System.out.println("Значение переменной hundred с типом byte равно " + hundred);
         System.out.println("Значение переменной thousand с типом short равно " + thousand);
         System.out.println("Значение переменной trillion с типом int равно " + trillion);
         System.out.println("Значение переменной smallDecimal с типом float равно " + smallDecimal);
         System.out.println("Значение переменной bigDecimal с типом double равно " + bigDecimal);
+        System.out.println(" ");
+
 
         //Задание 2
         //27.12 ,  987 678 965 549 , 2.786 , false, 569 , -159 , 27897 , 67
         //Объявите переменные всех типов, присвойте им любые значения (инициализируйте их).
+
         float numF = 27.12f;
         long numl = 987678965549L;
         byte numBt = 67;
@@ -33,19 +37,22 @@ public class Main {
         int numIt = 27897;
         double numDbl=2.786;
 
+
         //Задание 3
         //Трое школьных учителей, Людмила Павловна, Анна Сергеевна и Екатерина Андреевна, ведут три класса.
         //У Людмилы Павловны 23 ученика , у Анны Сергеевны 27 учеников и у Екатерины Андреевны – 30 учеников.
         //Три учительницы закупили все вместе 480 листов бумаги на все три класса.
         // Посчитайте, сколько достанется листов каждому ученику.
         //Результат задачи выведите в консоль в формате "На каждого ученика рассчитано … листов бумаги".
-        int teacherLP = 23;
-        int teacherAS = 27;
-        int teacherEA = 30;
-        int sumList = 480;
-        int sumChild=teacherEA+teacherAS+teacherLP;
-        int listChild=sumList/sumChild;
+        short teacherLP = 23;
+        short teacherAS = 27;
+        short teacherEA = 30;
+        short sumList = 480;
+        short sumChild= (short) (teacherEA+teacherAS+teacherLP);
+        short listChild= (short) (sumList/sumChild);
+        System.out.println("Задание 3 ");
         System.out.println("На каждого ученика рассчитано "+ listChild+ " листов бумаги..." );
+        System.out.println(" ");
 
         //Задание 4
         // Производительность машины для изготовления бутылок - 16 бутылок за 2 минуты. Какая производительность машины будет:
@@ -57,14 +64,14 @@ public class Main {
         //Рассчитывайте продолжительность работы машины в том случае, если она работает без перерыва заданный промежуток времени.
         //
         //Результаты подсчетов выведите в консоль в формате "За … машины произвела бутылок … штук "
-
+        System.out.println("Задание 4 ");
         byte min2 = 16;
-        int min20 = 20*min2;
+        short min20 = (short) (20*min2);
         System.out.println("За 20 минут машины произвела бутылок "+min20+" штук");
-        int dayMin=24*60;
-        int day1=dayMin*min2;
-        System.out.println("За сутки машины произвела бутылок "+day1+" штук");
-        int day3=dayMin*3*60;
+        short dayMin=24*60;
+        short day1= (short) (dayMin*min2);
+        System.out.println("За 1 сутки машины произвела бутылок "+day1+" штук");
+        int day3=dayMin*3*60*min2;
         System.out.println("За 3 дня машины произвела бутылок "+day3+" штук");
         int day28=dayMin*28*60*min2;
         System.out.println("За месяц состоящий из 28 дней машины произвела бутылок "+day28 +" штук");
@@ -74,6 +81,7 @@ public class Main {
         System.out.println("За месяц состоящий из 30 дней машины произвела бутылок "+day30 +" штук");
         int day31=dayMin*31*60*min2;
         System.out.println("За месяц состоящий из 31 дня машины произвела бутылок "+day31 +" штук");
+        System.out.println(" ");
 
         //Задача 5.
         //На ремонт школы нужно 120 банок краски двух цветов: белой и коричневой.
@@ -82,7 +90,23 @@ public class Main {
         //
         //Выведите результат задачи в консоль в формате
         // "В школе, где … классов, нужно … банок белой краски и … банок коричневой краски".
+        System.out.println("Задание 5 ");
+        byte scoolRoom=120;
+        byte wait=2;
+        byte broun=4;
+        byte room1= (byte) (wait+broun);
+        byte totalRoom= (byte) (scoolRoom/room1);
+        byte sumWait= (byte) (totalRoom*wait);
+        byte sumBraun= (byte) (totalRoom*broun);
+        System.out.println("В школе, где "+totalRoom+" классов, нужно "+sumWait+" банок белой краски и "+sumBraun+" банок коричневой краски");
+        System.out.println("");
 
+        System.out.println("Задание 6 ");
+        System.out.println("");
 
+        System.out.println("Задание 7 ");
+        System.out.println("");
+        System.out.println("Задание 8 ");
+        System.out.println("");
     }
 }
